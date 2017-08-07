15904a57941cd935387ad165eb81acf8f9356e18
HywelXu <hywelxu@126.com>
2017-08-07 15:11:21 +0800

    make banner invisible

--- app/src/main/java/org/hywel/kotlintest/ui/MainActivity.kt
+++ app/src/main/java/org/hywel/kotlintest/ui/MainActivity.kt
@@ -27,7 +27,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setBanner()
+//        setBanner()
         setRecyclerView()
     }
 
